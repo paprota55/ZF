629b2dae4618d19e0268d42ad8e4e612e667ab54
paprota55 <paprota55@gmail.com>
2021-05-18 23:37:54 +0200

    Prepared SwaggerConfig and added annotations to API and DTOs.

--- src/main/java/pl/paprota/zf/config/SwaggerConfig.java
+++ src/main/java/pl/paprota/zf/config/SwaggerConfig.java
@@ -9,21 +9,33 @@ import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 
+import java.util.Collections;
+
 import static springfox.documentation.builders.PathSelectors.regex;
 
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
 
-//    @Bean
-//    public Docket SwaggerApi(){
-//        return new Docket(DocumentationType.SWAGGER_2)
-//                .groupName("codecouple-api")
-//                .apiInfo(apiInfo())
-//                .select()
-//                .apis(RequestHandlerSelectors.any())
-//                .paths(getSwaggerPaths())
-//                .build();
-//    }
-//
+    @Bean
+    public Docket SwaggerApi(){
+        return new Docket(DocumentationType.OAS_30)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("pl.paprota.zf.controllers"))
+                .build()
+                .apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo(){
+        return new ApiInfo(
+                "List Employee API",
+                "API where you can add employees and postal addresses",
+                "1.0",
+                "Free to use",
+                new springfox.documentation.service.Contact("Rafał Paprota", "https://github.com/paprota55", "samplemail@gmail.com"),
+                "Free to use",
+                "",
+                Collections.emptyList());
+    }
+
 }
